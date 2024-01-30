61a8accf51c0a084255856d51eb1e5a423522cea
Ashmit Shukla <ashmitshukla18@gmail.com>
2024-01-30 22:00:24 +0530

    Working on Form and Slug Values

--- src/components/AuthLayout.jsx
+++ src/components/AuthLayout.jsx
@@ -1,18 +1,15 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-import { UseSelector, useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
+import React from "react";
+import { useState, useEffect } from "react";
+import { UseSelector, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
+export default function Protected({ children, authentication = true }) {
+  const navigate = useNavigate();
+  const [loader, setLoader] = useState(true);
+  const authStatus = useSelector((state) => state.auth.status);
 
-export default function Protected({
-      children, authentication = true
-}) {
-
-      const navigate = useNavigate();
-      const [loader, setLoader] = useState(true);
-      const authStatus = useSelector(state => state.auth.status)
-
-      useEffect(() => {
+  useEffect(
+    () => {
       // TODO: make it more easy to understand
 
       //   if (authStatus ===true){
@@ -20,24 +17,19 @@ export default function Protected({
       //   } else if (authStatus === false) {
       //       navigate("/login")
       //   }
-        
+
       //   let authValue = authStatus === true ? true : false
 
-      if(authentication && authStatus !== authentication)
-      {
-            navigate("/login");
-      }
-      else if(!authentication && authStatus !== authentication)
-      {
-            navigate("/");
+      if (authentication && authStatus !== authentication) {
+        navigate("/login");
+      } else if (!authentication && authStatus !== authentication) {
+        navigate("/");
       }
       setLoader(false);
-      }, [authStatus, navigate], authentication)
+    },
+    [authStatus, navigate],
+    authentication
+  );
 
-  return (
-
-      loader ? <h1>Loading...</h1> : <>{children}</>
-
-      
-  )
+  return loader ? <h1>Loading...</h1> : <>{children}</>;
 }
